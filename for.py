b2d6ca7832f9ae6573ab4d248c63733ab1dd5e9e
kimllk <kimllk@naver.com>
2024-02-08 20:58:50 +0900

    for문

---
+++ for.py
@@ -0,0 +1,17 @@
+numbers = [1,2,3,4,5]
+total = 0
+
+for number in numbers:
+    total += number
+    print(total)
+    
+cart = [1,2,"과일","음료수"]
+
+for i in cart:
+    print(i)
+    
+num = 0
+
+while num<5:
+    print(num)
+    num = num + 1
