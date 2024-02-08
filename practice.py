079c33e79d32098c04251b98ca3eeeefeaf0915c
kimllk <159398993+kimllk@users.noreply.github.com>
2024-02-08 17:07:33 +0900

    second commit

---
+++ practice.py
@@ -0,0 +1,33 @@
+#리스트 (시작)
+names = ['mike','jane', 'tom']
+print(names)
+
+print(names[0])
+print(names[2])
+# 리스트 (끝)
+
+#사전(딕셔너리 시작)
+user = {}
+user['age'] = 25
+user['address'] = 'seoul'
+
+print(user)
+print(user['age'])
+print(user.get('age'))
+user['age'] = 90
+print(user.get('age'))
+
+#사전(딕셔너리 끝)
+
+#홀수만 5개 있는 리스트 만들어서 출력 (시작)
+odd = [1,3,5,7,9]
+print(odd)
+#홀수만 5개 있는 리스트 만들어서 출력 (끝)
+
+#도시이름 3개 넣은 리스트 만들어서 출력(시작)
+city = ['춘천','서울','정선']
+print(city)
+#도시이름 3개 넣은 리스트 만들어서 출력(끝)
+
+#과일(fruit) 사전을 만들어보세요 키는 color, size로 해주세요(시작)
+#과일(fruit) 사전을 만들어보세요 키는 color, size로 해주세요(끝)
